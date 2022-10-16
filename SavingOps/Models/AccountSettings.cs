abc3c7a7f87bd286df2c5ec66892a715f3aaf9ca
Riley Howley <71965268+Riley-Howley@users.noreply.github.com>
2022-10-16 14:04:58 +1300

    Completed

--- SavingOps/Models/AccountSettings.cs
+++ SavingOps/Models/AccountSettings.cs
@@ -5,8 +5,10 @@
         [Key]
         public int AccountSettingsID { get; set; }
         [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:0.00}")]
         public double RentPrice { get; set; }
         [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:0.00}")]
         public double SavingsGoal { get; set; }
     }
 }
